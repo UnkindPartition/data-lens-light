b106b29881c7ead40605cc0e79160a0a1925f192
Roman Cheplyaka <roma@ro-che.info>
2014-04-28 23:09:12 +0300

    Clarify the docs for strict operators

--- src/Data/Lens/Light/State.hs
+++ src/Data/Lens/Light/State.hs
@@ -15,15 +15,25 @@ access :: MonadState a m => Lens a b -> m b
 access l = gets (getL l)
 
 -- | Set a value using a lens into state
-(~=), (!=) :: MonadState a m => Lens a b -> b -> m ()
+(~=) :: MonadState a m => Lens a b -> b -> m ()
 l ~= b = modify $ setL l b
+
+-- | Set a value using a lens into state. Forces both the value and the
+-- whole state.
+(!=) :: MonadState a m => Lens a b -> b -> m ()
 l != b = modify' $ setL l $! b
+
 infixr 4 ~=, !=
 
 -- | Infix modification of a value through a lens into state
-(%=), (!%=) :: MonadState a m => Lens a b -> (b -> b) -> m ()
+(%=) :: MonadState a m => Lens a b -> (b -> b) -> m ()
 l %= f = modify $ modL l f
+
+-- | Infix modification of a value through a lens into state. Forces both
+-- the function application and the whole state.
+(!%=) :: MonadState a m => Lens a b -> (b -> b) -> m ()
 l !%= f = modify' $ modL' l f
+
 infixr 4 %=, !%=
 
 modify' :: MonadState s m => (s -> s) -> m ()
