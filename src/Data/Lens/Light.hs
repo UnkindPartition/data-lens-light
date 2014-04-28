5295e3b23ae7b944a5f7f081fd4572d12235e40b
Roman Cheplyaka <roma@ro-che.info>
2014-04-28 18:18:45 +0300

    Initial

---
+++ src/Data/Lens/Light.hs
@@ -0,0 +1,48 @@
+module Data.Lens.Light
+  ( Lens(..)
+  , lens
+  , iso
+  , getL
+  , setL
+  , modL
+  , (^.)
+  )
+  where
+
+import Prelude hiding (id, (.))
+import Control.Category
+
+newtype Lens a b = Lens { runLens :: a -> (b -> a, b) }
+
+instance Category Lens where
+  id = iso id id
+  x . y =
+    lens
+      (getL x . getL y)
+      (\b -> modL y $ setL x b)
+
+-- | Build a lens out of a getter and setter
+lens :: (a -> b) -> (b -> a -> a) -> Lens a b
+lens get set = Lens $ \a -> (flip set a, get a)
+
+-- | Build a lens out of an isomorphism
+iso :: (a -> b) -> (b -> a) -> Lens a b
+iso f g = lens f (\x _ -> g x)
+
+-- | Get the getter function from a lens
+getL :: Lens a b -> a -> b
+getL l = snd . runLens l
+
+-- | Get the setter function from a lens
+setL :: Lens a b -> b -> a -> a
+setL l = flip $ fst . runLens l
+
+-- | Get the modifier function from a lens
+modL :: Lens a b -> (b -> b) -> a -> a
+modL l f a =
+  case runLens l a of
+    (setx, x) -> setx (f x)
+
+-- | Infix version of 'getL' (with the reverse order of the arguments)
+infixl 9 ^.
+(^.) = flip getL
