ceb195601e1816a735e6208e54e60dd0efcefb5b
Roman Cheplyaka <roma@ro-che.info>
2014-05-26 16:38:10 +0300

    Make TH code more readable.

--- src/Data/Lens/Light/Template.hs
+++ src/Data/Lens/Light/Template.hs
@@ -3,7 +3,7 @@
 {- |
 This module provides an automatic Template Haskell
 routine to scour data type definitions and generate
-accessor objects for them automatically.
+lenses for them automatically.
 -}
 module Data.Lens.Light.Template (
    nameMakeLens, makeLenses, makeLens
@@ -18,7 +18,7 @@ import Data.Lens.Light.Core
 -- |@makeLenses n@ where @n@ is the name of a data type
 -- declared with @data@ looks through all the declared fields
 -- of the data type, and for each field beginning with an underscore
--- generates an accessor of the same name without the underscore.
+-- generates a lens of the same name without the underscore.
 --
 -- It is "nameMakeLens" n f where @f@ satisfies
 --
@@ -44,7 +44,7 @@ import Data.Lens.Light.Core
 --
 -- > $( makeLenses [''TypeName] )
 --
--- And will generate accessors when TypeName was declared
+-- And will generate lenses when TypeName was declared
 -- using @data@ or @newtype@.
 makeLenses :: [Name] -> Q [Dec]
 makeLenses = liftM concat . mapM makeLens
@@ -70,8 +70,8 @@ namedFields _ = []
 
 -- |@nameMakeLens n f@ where @n@ is the name of a data type
 -- declared with @data@ and @f@ is a function from names of fields
--- in that data type to the name of the corresponding accessor. If
--- @f@ returns @Nothing@, then no accessor is generated for that
+-- in that data type to the name of the corresponding lens If
+-- @f@ returns @Nothing@, then no lens is generated for that
 -- field.
 nameMakeLens :: Name -> (String -> Maybe String) -> Q [Dec]
 nameMakeLens t namer = do
@@ -81,58 +81,72 @@ nameMakeLens t namer = do
                     _ -> fail $ errmsg t
     decMakeLens t reified namer
 
+-- | This function accepts a data type or newtype declaration, and generates
+-- the definitions based on that.
 decMakeLens :: Name -> Dec -> (String -> Maybe String) -> Q [Dec]
 decMakeLens t dec namer = do
     (params, cons) <- case dec of
                  DataD _ _ params cons' _ -> return (params, cons')
                  NewtypeD _ _ params con' _ -> return (params, [con'])
                  _ -> fail $ errmsg t
-    decs <- makeAccs params . nub $ concatMap namedFields cons
+    decs <- declareLenses params . nub $ concatMap namedFields cons
     when (null decs) $ qReport False nodefmsg
     return decs
 
     where
 
-    nodefmsg = "Warning: No accessors generated from the name " ++ show t
-          ++ "\n If you are using makeLenses rather than"
-          ++ "\n nameMakeLens, remember accessors are"
+    nodefmsg = "Warning: No lenses generated from the name " ++ show t
+          ++ "\n If you are using declareLenses rather than"
+          ++ "\n nameMakeLens, remember lenses are"
           ++ "\n only generated for fields starting with an underscore"
 
-    makeAccs :: [TyVarBndr] -> [VarStrictType] -> Q [Dec]
-    makeAccs params vars =
-        liftM (concat . catMaybes) $ mapM (\ (name,_,ftype) -> makeAccFromName name params ftype) vars
+    declareLenses
+      :: [TyVarBndr] -- type variables to which the type type constructor
+                     -- should be applied
+      -> [VarStrictType] -- record fields 
+      -> Q [Dec]
+    declareLenses params vars =
+        liftM (concat . catMaybes) $ mapM (\ (name,_,ftype) -> declareLensFromName name params ftype) vars
 
+    -- | Create a TH name for the lens based on the corresponding field
+    -- name
     transformName :: Name -> Maybe Name
     transformName (Name occ _) = do
         n <- namer (occString occ)
         return $ Name (mkOccName n) NameS
 
-    makeAccFromName :: Name -> [TyVarBndr] -> Type -> Q (Maybe [Dec])
-    makeAccFromName name params ftype =
+    -- | Declare a lens knowing only the field name, but not the lens name
+    --
+    -- (Or do not declare a lens at all, if we're told so by 'namer'.)
+    declareLensFromName :: Name -> [TyVarBndr] -> Type -> Q (Maybe [Dec])
+    declareLensFromName name params ftype =
         case transformName name of
             Nothing -> return Nothing
-            Just n -> liftM Just $ makeAcc name params ftype n
+            Just n -> liftM Just $ declareLens name params ftype n
 
-    makeAcc ::Name -> [TyVarBndr] -> Type -> Name -> Q [Dec]
-    makeAcc name params ftype accName = do
+    -- | Declare a lens knowing both the field name and the lens name.
+    --
+    -- This function performs actual code generation.
+    declareLens :: Name -> [TyVarBndr] -> Type -> Name -> Q [Dec]
+    declareLens fieldName params ftype lensName = do
         let params' = map (\x -> case x of (PlainTV n) -> n; (KindedTV n _) -> n) params
         let appliedT = foldl AppT (ConT t) (map VarT params')
         body <- [|
                  lens
-                    ( $( return $ VarE name ) )
+                    ( $( return $ VarE fieldName ) )
                     ( \x s ->
-                        $( return $ RecUpdE (VarE 's) [(name, VarE 'x)] ) )
+                        $( return $ RecUpdE (VarE 's) [(fieldName, VarE 'x)] ) )
                 |]
         return
-          [ SigD accName (ForallT (map PlainTV params')
+          [ SigD lensName (ForallT (map PlainTV params')
                [] (AppT (AppT (ConT ''Lens) appliedT) ftype))
-          , ValD (VarP accName) (NormalB body) []
+          , ValD (VarP lensName) (NormalB body) []
           ]
 
 errmsg :: Show a => a -> [Char]
-errmsg t = "Cannot derive accessors for name " ++ show t ++ " because"
+errmsg t = "Cannot derive lenses for name " ++ show t ++ " because"
          ++ "\n it is not a type declared with 'data' or 'newtype'"
          ++ "\n Did you remember to double-tick the type as in"
-         ++ "\n $(makeLenses ''TheType)?"
+         ++ "\n $(declareLenses ''TheType)?"
 
 
